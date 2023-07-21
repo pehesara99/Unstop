423738d8a3436972420ec3a24fd65a8e6e7cf588
Paduma98 <padumahitihami@gmail.com>
2023-07-21 21:05:16 +0530

    add main project

---
+++ shopping-site-microservice/inventory-service/src/main/java/com/unstop/inventoryservice/InventoryApplication.java
@@ -0,0 +1,12 @@
+package com.unstop.inventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+}
---
+++ shopping-site-microservice/order-service/src/main/java/com/unstop/orderservice/config/ManualConfiguration.java
@@ -0,0 +1,20 @@
+package com.unstop.orderservice.config;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+
+
+@Configuration(proxyBeanMethods = false)
+@RequiredArgsConstructor
+public class ManualConfiguration {
+
+    private final KafkaTemplate kafkaTemplate;
+
+    @PostConstruct
+    void setup() {
+        this.kafkaTemplate.setObservationEnabled(true);
+    }
+
+}
